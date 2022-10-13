9dda4bc5a06a99af4ebf806adbab89d0c472e0a1
Charles Eze <cobinna11@gmail.com>
2022-10-13 09:27:34 -0700

    The first C language Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - The Entry for my program
+ *
+ * Description: This is meant to be a longer description
+ * Return: return the value 0 for a success code
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
