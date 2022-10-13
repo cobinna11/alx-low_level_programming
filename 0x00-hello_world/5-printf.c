da314416340d6ab81fbea3b808a8ce1a6adaaf9c
Charles Eze <cobinna11@gmail.com>
2022-10-13 09:43:45 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point for my program
+ *
+ * Description: This is the the one that runs everything
+ * Return: The value that returns 0 for code success
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
