f0a7e0880d30b64d6c0c6a7d7af3add6a44ed73a
Charles Eze <cobinna11@gmail.com>
2022-10-14 04:38:20 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,28 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there 
- * main - This is the way to check if it assigns random numbers to int
- * it runs and executes it
- * Return: the value is 0 for code success*/
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n>5);
-{
-	printf("Last digit of n is %d : and is greater than 5");
-}
-	if (n==0);
-{
-	printf("Last digit of n is %d : and is 0");
-}
-	if (n<6!0);
-{
-	printf("Last digit of n is %d : and is less than 6 and not 0");
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
 	return (0);
-}
 }
