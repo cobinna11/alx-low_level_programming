983dddc8d7ffdc961c58b9e3494c131d1a8f9d4b
Charles Eze <cobinna11@gmail.com>
2022-10-20 13:20:42 -0700

    Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -12,9 +12,9 @@ void print_numbers(void)
 {
 	int number;
 
-	for (number = '0'; number <= '9'; number++)
+	for (number = 0; number <= 9; number++)
 	{
-		_putchar(number);
+		_putchar(number + "0");
 	}
 	_putchar('\n');
 }
