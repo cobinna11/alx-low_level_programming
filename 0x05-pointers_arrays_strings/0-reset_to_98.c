6f456f6dc12c64078160f4f2d181928f86c95b45
Charles Eze <cobinna11@gmail.com>
2022-10-24 04:09:21 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,27 +1,11 @@
 #include "main.h"
 
 /**
- * _strcat - function with two argument
- * @dest: char type pointer 1st argument
- * @src: char type pointer 2nd argument
- *
- * Description: concatenates two strings
- * Return: na
+ * reset_to_98 - updates the value *n points to.
+ * @n: the number to modify.
  */
-char *_strcat(char *dest, char *src)
-{
-	char *start = dest;
 
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (start);
+void reset_to_98(int *n)
+{
+	*n = 98;
 }
