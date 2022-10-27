0b64ea27dee509183b6d55691c7d93bad2ae7433
Charles Eze <cobinna11@gmail.com>
2022-10-27 09:38:11 -0700

    Tasks are done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char c);
+
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
@@ -16,4 +18,5 @@ char *string_toupper(char *)
 char *cap_string(char *);
 
 char *leet(char *);
+
 #endif
